58524cb7b7aa59eb991d998923e66ca76512daa0
Vinicius Dhenner <viniciusefisim@gmail.com>
2025-01-31 16:33:51 -0400

    executando

---
+++ src/main/java/com/sge/igrejas/IgrejasApplication.java
@@ -0,0 +1,11 @@
+package com.sge.igrejas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IgrejasApplication.class, args);
+    }
+}
